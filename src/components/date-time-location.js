d2da706cb7f6e14e38d601771f006a6e9c3f6c90
Valentina Kochegarova <kochegarova_v@yahoo.com>
2022-01-30 21:46:57 +0100

    date-time-location.js icon size

--- src/components/date-time-location.js
+++ src/components/date-time-location.js
@@ -7,15 +7,15 @@ export default function DateTimeLocation() {
   return(
     <div className={styles.container}>
       <div className={styles.icon}>
-        <FontAwesomeIcon icon={faCalendarAlt}/>
+        <FontAwesomeIcon icon={faCalendarAlt} className="fas fa-camera fa-2x"/>
         <h6>May 20 <br /> 2022</h6>
       </div>
       <div className={styles.icon}>
-        <FontAwesomeIcon icon={faClock}/>
+        <FontAwesomeIcon icon={faClock} className="fas fa-camera fa-2x" />
         <h6>13:00 - 17:00 CET</h6>
       </div>
       <div className={styles.icon}>
-        <FontAwesomeIcon icon={faMapMarkerAlt}/>
+        <FontAwesomeIcon icon={faMapMarkerAlt} className="fas fa-camera fa-2x" />
         <h6>Aula Medica <br />Stockholm</h6>
       </div>
     </div>
